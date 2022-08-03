3b5cd383d91c520e3e29b684ca635cf632564222
Melody Y <meloyang@amazon.com>
2022-08-03 22:55:48 +1000

    update jam_asset

--- jam_asset/submit_emr_on_eks.sh
+++ jam_asset/submit_emr_on_eks.sh
@@ -9,8 +9,8 @@ aws emr-containers start-job-run \
   --release-label emr-6.5.0-latest \
   --job-driver '{
     "sparkSubmitJobDriver": {
-      "entryPoint": "s3://'${s3Bucket}'/app_code/job/wordcount.py","entryPointArguments":["s3://amazon-reviews-pds/parquet/","s3://'${s3Bucket}'/output/"], 
-      "sparkSubmitParameters": "--conf spark.executor.memory=4G --conf spark.executor.cores=1"}}' \
+      "entryPoint": "s3://'${s3Bucket}'/wordcount.py","entryPointArguments":["s3://amazon-reviews-pds/parquet/","s3://'${s3Bucket}'/output"], 
+      "sparkSubmitParameters": "--conf spark.driver.memory=1G --conf spark.driver.cores=1 --conf spark.executor.memory=6G --conf spark.executor.cores=1"}}' \
   --configuration-overrides='{
   	"applicationConfiguration": [
       {
@@ -20,7 +20,7 @@ aws emr-containers start-job-run \
           "spark.dynamicAllocation.shuffleTracking.enabled":"true",
           "spark.dynamicAllocation.maxExecutors":"30",
           "spark.kubernetes.allocation.batch.size": "15"
-         }
+        }
       }
     ]
   }'
