5dcff94a2f771e3f84513dda9edf22282698666c
Melody Y <meloyang@amazon.com>
2022-08-05 04:53:14 +1000

    reduce executor to 5

--- deployment/app_code/job/wordcount.py
+++ deployment/app_code/job/wordcount.py
@@ -2,5 +2,5 @@ import sys
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('Amazon reviews word count').getOrCreate()
 df = spark.read.parquet(sys.argv[1])
-df.select("product_category").groupBy("product_category").count().show(50)
+df.select("product_category").groupBy("product_category").count().show()
 exit()
