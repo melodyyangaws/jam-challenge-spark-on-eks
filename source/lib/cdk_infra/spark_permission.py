7638c05d652ce6445dca112c1124ac3899265e6b
syutingtaro <syuting@amazon.com>
2023-08-23 16:24:03 +0000

    unmaek EMR on EKS parts

--- source/lib/cdk_infra/spark_permission.py
+++ source/lib/cdk_infra/spark_permission.py
@@ -100,76 +100,76 @@ class SparkOnEksSAConst(Construct):
         for statmnt in _native_spark_iam:
             self._spark_sa.add_to_principal_policy(iam.PolicyStatement.from_json(statmnt))
 
-        # ########################################
-        # #######                          #######
-        # #######     EMR on EKS Assets    #######
-        # #######                          #######
-        # ########################################
-        # _emr_01_name = "emr"
-        # emr_ns = eks_cluster.add_manifest('EMRNamespace',{
-        #         "apiVersion": "v1",
-        #         "kind": "Namespace",
-        #         "metadata": { 
-        #             "name":  _emr_01_name,
-        #             "labels": {"name": _emr_01_name}
-        #         }
-        #     }
-        # ) 
-        # # k8s rbac for EMR on EKS
-        # _emr_rb = KubernetesManifest(self,'EMRRoleBinding',
-        #     cluster=eks_cluster,
-        #     manifest=load_yaml_replace_var_local(source_dir+'/app_resources/emr-rbac.yaml', 
-        #     fields= {
-        #         "{{NAMESPACE}}": _emr_01_name,
-        #     }, 
-        #     multi_resource=True)
-        # )
-        # _emr_rb.node.add_dependency(emr_ns)
+        ########################################
+        #######                          #######
+        #######     EMR on EKS Assets    #######
+        #######                          #######
+        ########################################
+        _emr_01_name = "emr"
+        emr_ns = eks_cluster.add_manifest('EMRNamespace',{
+                "apiVersion": "v1",
+                "kind": "Namespace",
+                "metadata": { 
+                    "name":  _emr_01_name,
+                    "labels": {"name": _emr_01_name}
+                }
+            }
+        ) 
+        # k8s rbac for EMR on EKS
+        _emr_rb = KubernetesManifest(self,'EMRRoleBinding',
+            cluster=eks_cluster,
+            manifest=load_yaml_replace_var_local(source_dir+'/app_resources/emr-rbac.yaml', 
+            fields= {
+                "{{NAMESPACE}}": _emr_01_name,
+            }, 
+            multi_resource=True)
+        )
+        _emr_rb.node.add_dependency(emr_ns)
         
-        # # EMR on EKS Execution Role 
-        # self._emr_exec_role = iam.Role(self, "EMRJobExecRole", assumed_by=iam.ServicePrincipal("eks.amazonaws.com"))
+        # EMR on EKS Execution Role 
+        self._emr_exec_role = iam.Role(self, "EMRJobExecRole", assumed_by=iam.ServicePrincipal("eks.amazonaws.com"))
         
-        # # trust policy
-        # _eks_oidc_provider=eks_cluster.open_id_connect_provider 
-        # _eks_oidc_issuer=_eks_oidc_provider.open_id_connect_provider_issuer 
+        # trust policy
+        _eks_oidc_provider=eks_cluster.open_id_connect_provider 
+        _eks_oidc_issuer=_eks_oidc_provider.open_id_connect_provider_issuer 
          
-        # sub_str_like = CfnJson(self, "ConditionJsonIssuer",
-        #     value={
-        #         f"{_eks_oidc_issuer}:sub": f"system:serviceaccount:{_emr_01_name}:emr-containers-sa-*-*-{Aws.ACCOUNT_ID}-*"
-        #     }
-        # )
-        # self._emr_exec_role.assume_role_policy.add_statements(
-        #     iam.PolicyStatement(
-        #         effect=iam.Effect.ALLOW,
-        #         actions=["sts:AssumeRoleWithWebIdentity"],
-        #         principals=[iam.OpenIdConnectPrincipal(_eks_oidc_provider, conditions={"StringLike": sub_str_like})])
-        # )
+        sub_str_like = CfnJson(self, "ConditionJsonIssuer",
+            value={
+                f"{_eks_oidc_issuer}:sub": f"system:serviceaccount:{_emr_01_name}:emr-containers-sa-*-*-{Aws.ACCOUNT_ID}-*"
+            }
+        )
+        self._emr_exec_role.assume_role_policy.add_statements(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["sts:AssumeRoleWithWebIdentity"],
+                principals=[iam.OpenIdConnectPrincipal(_eks_oidc_provider, conditions={"StringLike": sub_str_like})])
+        )
 
-        # aud_str_like = CfnJson(self,"ConditionJsonAudEMR",
-        #     value={
-        #         f"{_eks_oidc_issuer}:aud": "sts.amazon.com"
-        #     }
-        # )
-        # self._emr_exec_role.assume_role_policy.add_statements(
-        #     iam.PolicyStatement(
-        #         effect=iam.Effect.ALLOW,
-        #         actions=["sts:AssumeRoleWithWebIdentity"],
-        #         principals=[iam.OpenIdConnectPrincipal(_eks_oidc_provider, conditions={"StringEquals": aud_str_like})]
-        #     )
-        # )
+        aud_str_like = CfnJson(self,"ConditionJsonAudEMR",
+            value={
+                f"{_eks_oidc_issuer}:aud": "sts.amazon.com"
+            }
+        )
+        self._emr_exec_role.assume_role_policy.add_statements(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["sts:AssumeRoleWithWebIdentity"],
+                principals=[iam.OpenIdConnectPrincipal(_eks_oidc_provider, conditions={"StringEquals": aud_str_like})]
+            )
+        )
 
-        # # associate IAM Roles to EMR on EKS Service Accounts 
-        # for statmnt in _etl_iam:
-        #     self._emr_exec_role.add_to_policy(iam.PolicyStatement.from_json(statmnt))
+        # associate IAM Roles to EMR on EKS Service Accounts 
+        for statmnt in _etl_iam:
+            self._emr_exec_role.add_to_policy(iam.PolicyStatement.from_json(statmnt))
 
-        # # create EMR virtual Cluster Server
-        # self.emr_vc = emrc.CfnVirtualCluster(self,"EMRVC",
-        #     container_provider=emrc.CfnVirtualCluster.ContainerProviderProperty(
-        #         id=eks_cluster.cluster_name,
-        #         info=emrc.CfnVirtualCluster.ContainerInfoProperty(eks_info=emrc.CfnVirtualCluster.EksInfoProperty(namespace=_emr_01_name)),
-        #         type="EKS"
-        #     ),
-        #     name="emr-on-eks-demo"
-        # )
-        # self.emr_vc.node.add_dependency(self._emr_exec_role)
-        # self.emr_vc.node.add_dependency(_emr_rb)     
+        # create EMR virtual Cluster Server
+        self.emr_vc = emrc.CfnVirtualCluster(self,"EMRVC",
+            container_provider=emrc.CfnVirtualCluster.ContainerProviderProperty(
+                id=eks_cluster.cluster_name,
+                info=emrc.CfnVirtualCluster.ContainerInfoProperty(eks_info=emrc.CfnVirtualCluster.EksInfoProperty(namespace=_emr_01_name)),
+                type="EKS"
+            ),
+            name="emr-on-eks-demo"
+        )
+        self.emr_vc.node.add_dependency(self._emr_exec_role)
+        self.emr_vc.node.add_dependency(_emr_rb)     
