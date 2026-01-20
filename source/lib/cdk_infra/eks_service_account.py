0f0e9487507dc03104b7ef0f87d77d089ad46b94
Melody <56663770+melodyyangaws@users.noreply.github.com>
2026-01-21 01:41:21 +1100

    Reorganize service account creation for dependencies
    Reordered service account creation to ensure dependencies are respected. The ALB Ingress service account is now created before the Cluster Auto-scaler and External Secret Controller.

--- source/lib/cdk_infra/eks_service_account.py
+++ source/lib/cdk_infra/eks_service_account.py
@@ -19,17 +19,8 @@ class EksSAConst(Construct):
 # //***********************************************************************************************//
         source_dir=os.path.split(os.environ['VIRTUAL_ENV'])[0]+'/source'
          
-         # Cluster Auto-scaler
-        self._scaler_sa = eks_cluster.add_service_account('AutoScalerSa', 
-            name='cluster-autoscaler', 
-            namespace='kube-system'
-        )  
-        _scaler_role = load_yaml_local(source_dir+'/app_resources/autoscaler-iam-role.yaml')
-        for statmt in _scaler_role:
-            self._scaler_sa.add_to_principal_policy(iam.PolicyStatement.from_json(statmt))
-
-        # ALB Ingress
-        self._alb_sa = eks_cluster.add_service_account('ALBServiceAcct', 
+        # ALB Ingress - Create first since other components depend on it
+        self._alb_sa = eks_cluster.add_service_account('ALBServiceAcct',
             name='alb-aws-load-balancer-controller',
             namespace='kube-system'
         )
@@ -37,14 +28,24 @@ class EksSAConst(Construct):
         for statmt in _alb_role:
             self._alb_sa.add_to_principal_policy(iam.PolicyStatement.from_json(statmt))
 
-        # External secret controller
+        # Cluster Auto-scaler - Create after ALB to serialize operations
+        self._scaler_sa = eks_cluster.add_service_account('AutoScalerSa', 
+            name='cluster-autoscaler', 
+            namespace='kube-system'
+        )  
+        self._scaler_sa.node.add_dependency(self._alb_sa)
+        _scaler_role = load_yaml_local(source_dir+'/app_resources/autoscaler-iam-role.yaml')
+        for statmt in _scaler_role:
+            self._scaler_sa.add_to_principal_policy(iam.PolicyStatement.from_json(statmt))
+
+        # External secret controller - Create last to serialize operations
         self._secrets_sa = eks_cluster.add_service_account('ExSecretController',
             name='external-secrets-controller',
             namespace="kube-system"
         )
-        self._secrets_sa.node.add_dependency(secret)
+        self._secrets_sa.node.add_dependency(self._scaler_sa)
         _secrets_role = load_yaml_replace_var_local(source_dir+'/app_resources/ex-secret-iam-role.yaml',
                         fields={"{{secretsmanager}}": secret.secret_arn+"*"}
                     )
         for statmt in _secrets_role:
-            self._secrets_sa.add_to_principal_policy(iam.PolicyStatement.from_json(statmt))  
+            self._secrets_sa.add_to_principal_policy(iam.PolicyStatement.from_json(statmt))
